7cb8354cd64f8f747a8beb8c08d98468a883b337
Nils <40796807+uchars@users.noreply.github.com>
2026-01-19 19:55:03 +0100

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ TMPDIR="$(mktemp -d)"
 trap 'rm -rf "$TMPDIR"' EXIT
 
 echo "==> Cloning setup repo..."
-git clone --depth 1 https://github.com/sterzn/setup.git "$TMPDIR/setup"
+git clone --depth 1 https://github.com/uchars/setup.git "$TMPDIR/setup"
 
 echo "==> Running installer..."
 /bin/bash "$TMPDIR/setup/install.sh"
