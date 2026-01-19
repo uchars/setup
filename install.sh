3c7df18cc3c30f77f4269a2c43b4c8bcf60caa70
Nils <40796807+uchars@users.noreply.github.com>
2026-01-19 19:57:31 +0100

    update

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ source "$ROOT_DIR/system/system.sh"
 source "$ROOT_DIR/system/dwm.sh"
 source "$ROOT_DIR/system/npm.sh"
 log "Applying dotfiles"
-chezmoi init https://github.com/uchars/dotfiles.git
+chezmoi init git@github.com:uchars/dotfiles.git
 
 log "Done"
 
