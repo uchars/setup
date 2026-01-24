e4dae98a6a7d856b18f931748f9e20c3c4304228
Nils <40796807+uchars@users.noreply.github.com>
2026-01-24 22:56:01 +0100

    update

--- system/dwm.sh
+++ system/dwm.sh
@@ -19,3 +19,22 @@ if ! command_exists dwm; then
 else
     log "dwm already installed, skipping"
 fi
+
+DWMBLOCKS_DIR="$HOME/work/dwmblocks"
+if ! command_exists dwmblocks; then
+    log "Installing dwmblocks from source..."
+
+    # clone if not already cloned in temp folder
+    if [[ ! -d "$DWMBLOCKS_DIR" ]]; then
+        git clone https://github.com/uchars/dwmblocks-async.git "$DWMBLOCKS_DIR"
+    fi
+
+    pushd "$DWMBLOCKS_DIR" >/dev/null
+    make clean
+    sudo make install -j$(($(nproc)/2))
+    popd >/dev/null
+
+    log "dwmblocks installation complete"
+else
+    log "dwmblocks already installed, skipping"
+fi
