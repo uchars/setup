af0c69cff46c255e8866f6a36b1da4b5e76f80dc
Nils <40796807+uchars@users.noreply.github.com>
2026-07-17 09:21:23 +0200

    update

--- bootstrap/fedora.sh
+++ bootstrap/fedora.sh
@@ -11,11 +11,15 @@ ensure_sudo() {
 bootstrap_fedora() {
   ensure_sudo
 
+  sudo dnf update -y
+
   sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
-  PKGS_DNF=(chezmoi ripgrep fzf source-foundry-hack-fonts cmake libxcb-devel xcb-util-devel xcb-util-wm-devel alacritty python3-pip fastfetch make gcc krita opentoonz steam docker picom @base-x rofi xrandr dunst feh compat-ffmpeg4 golang ninja clang eglinfo git make gcc libXft-devel libX11-devel libXinerama-devel)
-
+  sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
   sudo dnf install -y ffmpeg --allowerasing
+  sudo dnf install -y @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+  sudo dnf install -y mesa-va-drivers-freeworld
+
+  PKGS_DNF=(chezmoi ripgrep fzf source-foundry-hack-fonts cmake libxcb-devel xcb-util-devel xcb-util-wm-devel alacritty python3-pip fastfetch make gcc krita opentoonz steam docker picom @base-x rofi xrandr dunst feh compat-ffmpeg4 golang ninja clang eglinfo git make gcc libXft-devel libX11-devel libXinerama-devel xorg-x11-server-Xorg xorg-x11-xinit)
 
   log "Updating dnf repositories"
   sudo dnf makecache --refresh || true
--- system/flatpak.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-source "$ROOT_DIR/scripts/helpers.sh"
-
-FLATPAKS=(com.bitwarden.desktop org.kde.krita com.discordapp.Discord com.github.jeromerobert.pdfarranger com.visualstudio.code com.yubico.yubioath org.gnome.Extensions com.usebottles.bottles com.google.AndroidStudio org.kde.haruna io.github.OpenToonz com.valvesoftware.Steam)
-install_flatpaks "${FLATPAKS[@]}" || log "Some flatpak installs failed or were skipped"
